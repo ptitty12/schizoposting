2e5640515cbc1dd2600a64891cb63a2142cc6a50
Patrick Taylor <SESA651106@se.com>
2024-09-29 20:47:07 -0500

    Golly

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB limit
-    my_key_1 = 'sk-proj-28wh3-gKOSLWt-HFki88uK3Tcf6Zunb1s1iOdk9RzpUNA3waBApzLsdVd4thMIb7'
-    my_key_2 = 'VVRQsZ6fsdT3BlbkFJcCpWfIs3X7Kqd79op7heOsAZ_BPBbJ71hMwBtSlarQ9zHJ4HoEXEe_hEfjHCtKKxigv2SGlyQA'
+    my_key_1 = 'sk-proj-u5fLSdRphFLjigYJ8gls'
+    my_key_2 = 'QOafHrkodnUbiCNf2DkPYi3BW7z7FrZUH0Dgqw9DxxxIk5ZJj7qJnmT3BlbkFJ__kKgMoIi40DSSOlndk8jRb6arLlKXgjb7GPlfvfvuwxDxtI7jQBozm2w4bYAO7mvEl-5HNicA'
     OPENAI_API_KEY = my_key_1+my_key_2
