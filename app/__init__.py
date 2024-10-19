5ad5e946a8573e2aca5b63218757db2233457cb2
Patrick Taylor <paty.taylro12@gmail.com>
2024-10-19 14:00:18 -0500

    need to migrate db during this

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 import openai
 import os
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 def create_app():
     app = Flask(__name__, template_folder=os.path.abspath('templates'))
     app.config.from_object(Config)
     app.config['MAX_CONTENT_LENGTH'] = 55 * 1024 * 1024  # 16 MB limit
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Set OpenAI API key
     openai.api_key = app.config['OPENAI_API_KEY']
--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    ip_address = db.Column(db.String(45), nullable=True)
